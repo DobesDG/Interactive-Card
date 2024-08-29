3f213976aa7760755e0c9310c417408a23ceefc0
Douglas Galvão <douglasgalvao06@gmail.com>
2024-08-28 22:25:13 -0300

    Styles forms

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import './App.css'
 import { useState } from 'react'
-import { Input } from '../images/components/Input'
-import { Card } from '../images/components/Card'
+import { Input } from './components/Input'
+import { Card } from './components/Card'
 
 function App() {
 const [number,setNumber] = useState([""])
 const [name, setName] = useState([""])
 const [month,setMonth] = useState([""])
 const [year,setYear] = useState([""])
-const [cvv,setCvv] = useState([""])
+const [cvc,setCvc] = useState([""])
 
 const handleCardNumberChange = (e) => {
   let value = e.target.value.replace(/\D/g, '');
@@ -30,7 +30,7 @@ const handleCardNumberChange = (e) => {
               </div>
             </div>
             <div className='card-back'>
-              <Card defaultValue={'000'} value={cvv} setValue={setCvv} className={'card-cvv'}/>
+              <Card defaultValue={'000'} value={cvc} setValue={setCvc} className={'card-cvc'}/>
             </div>
           </section>
         </aside>
@@ -39,21 +39,28 @@ const handleCardNumberChange = (e) => {
             <form action="" className='form'>
               <Input title={'CARDHOLDER NAME'} value={name} setValue={setName} maxLength={'16'} minLength={'16'} type={'text'} placeholder={'e.g Jane Appleseed'}/>
               <Input title={'CARD NUMBER'} value={number} setValue={handleCardNumberChange} handle={true} maxLength={'19'} minLength={'19'} type={'text'} inputMode={"numeric"} placeholder={'e.g 1234 5678 9123 0000'}/>
-              <section className='expCvv-conteiner'>
+              <section className='expcvc-conteiner'>
                 <div className='expDate-conteiner'>
-                  <h5>EXP.DATE (MM/YY)</h5>
+                  <p>EXP. DATE (MM/YY)</p>
                   <div className='expDate-inputs'>
-                    <Input value={month} setValue={setMonth} maxLength={'2'} minLength={'2'} type={'text'} inputMode={"numeric"} placeholder={'MM'}/>
-                    <Input value={year} setValue={setYear} maxLength={'2'} minLength={'2'} type={'text'} inputMode={"numeric"} placeholder={'YY'}/>
+                    <div className='month-input'>
+                      <Input value={month} setValue={setMonth} maxLength={'2'} minLength={'2'} type={'text'} inputMode={"numeric"} placeholder={'MM'}/>
+                    </div>
+                    <div className='year-input'>
+                      <Input value={year} setValue={setYear} maxLength={'2'} minLength={'2'} type={'text'} inputMode={"numeric"} placeholder={'YY'}/>
+                    </div>
                   </div> 
                 </div>
-                <div className='cvv-conteiner'>
-                  <h5>CVV</h5>
-                  <div className='cvv-input'>
-                    <Input value={cvv} setValue={setCvv} maxLength={'3'} type={'text'} placeholder={'e.g 123'}/>
+                <div className='cvc-conteiner'>
+                  <p>CVC</p>
+                  <div className='cvc-input'>
+                    <Input value={cvc} setValue={setCvc} maxLength={'3'} type={'text'} placeholder={'e.g 123'}/>
                   </div>
                 </div>
               </section>
+                <div className='button-div'>
+                  <button className='submit-button'>Confirm</button>
+                </div>
             </form>
           </section>
         </section>
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,7 +1,7 @@
 export const Input = ({ value, setValue, required = true, type, maxLength, minLength, placeholder, handle = false , title, inputMode }) => {
     return (
         <div className="input-div">
-            <h5>{title}</h5>
+            <p>{title}</p>
             <input maxLength={maxLength} minLength={minLength} required={required} type={type} value={value} placeholder={placeholder} inputMode={inputMode} onChange={handle ? setValue : (e) => setValue(e.target.value)} />
         </div>
     );
